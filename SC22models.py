8d4dfa803bf0a993b6a9a736bd76e83ae5c5d0dc
spasca <simone.pascarosa@gmail.com>
2021-09-26 19:16:38 +0200

    Adding resource models

---
+++ SC22models.py
@@ -0,0 +1,45 @@
+class SC22Model:
+    content = dict()
+
+    # Default constructor that puts the result from SQL query inside content attribute
+    def __init__(self, ):
+        
+
+    # Depth attribute helps to correctly indent the JSON to be more readable
+    def toJSON(self,depth=1):
+        jsonstring = "{\n"
+        for key, value in self.content.items():
+            if isinstance(value, SC22Model):
+                jsonstring += ("  " * (depth)) + key + ": " + value.toJSON(depth+1) + ',\n'
+            else:
+                if isinstance(value, str):
+                    jsonstring += ("  " * (depth)) + key + ": " + '"' + str(value) + '",\n'
+                else:
+                    jsonstring += ("  " * (depth)) + key + ": " + str(value) + ',\n'
+        jsonstring = jsonstring[:-2] + "\n" # Remove trailing comma and newline
+        jsonstring += ("  " * (depth-1)) + "}"
+
+        return jsonstring
+
+
+class Campo(SC22Model):
+    pass
+
+class Gruppo(SC22Model):
+    def __init__(self, )
+
+    @classmethod
+    def buildFromSql(
+
+
+
+
+c = Campo()
+c.content = dict ({"idCampo": 1, "lat": 40.12, "lon": 10.21})
+
+g = Gruppo()
+g.content = dict( {"idGruppo": 1, "denominazione": "Parrocchia San Biagio e Savino", "campo": c})
+
+
+print (g.toJSON())
+
--- gruppo_adapter.py
+++ gruppo_adapter.py
@@ -1,14 +1,19 @@
 # gruppo_adapter.py
 # Adapter that exports DB query's to the WebAPI resource controller
 # 
+import 
 from db_interface import getConnection
 
+
 def getGruppo (id):
-    connection = getConnection()
-    
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM Gruppo")
-        for gruppi in cursor.fetchall():
-            print(gruppi)
+    # Enter the context of mysql connection with default config.ini
+    with getConnection() as connection:
+        # Get cursor to work with DB
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM Gruppo")
+            # ! Note that cursor.fetchall() returns a list of tuples
+            for gruppi in cursor.fetchall():
+                print(gruppi)
+            print(cursor.column_names)
 
 getGruppo(1)
